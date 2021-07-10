2ddf9d2cc3c3073fe789654ecd6434ab1c2d3ce5
julioandre <julioandre275@gmail.com>
2021-07-10 21:44:28 +0200

    Connected commercejs api and added Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from 'react'
-import Products from './components/Products'
+import React, {useState,useEffect} from 'react'
+
+import {commerce} from './lib/commerce';
+import {Products , Navbar, Cart} from './components';
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
 
 const App = () => {
+    const [products,setProducts] = useState([])
+    const [cart,setCart]= useState([])
+    const fetchProducts = async()=>{
+        const {data} = await commerce.products.list()
+        setProducts(data);
+    }
+
+    const fetchCart=async()=>{
+        
+        setCart(await commerce.cart.retrieve());
+    }
+    const handleAddToCart = async(productId, quantity)=>{
+        const item = await commerce.cart.add(productId,quantity)
+
+        setCart(item.cart)
+    }
+    useEffect(()=>{
+        fetchProducts();
+        fetchCart();
+    },[])
+
+    console.log(cart)
     return (
-        <div>
-            <Products />
-        </div>
-    )
+        <Router>
+             <div>
+                <Navbar totalItems={cart.total_items}/>
+                <Switch>
+                    <Route exact path>
+                        
+                    </Route>
+                    <Route>
+
+                    </Route>
+                </Switch>
+                {/* <Products products={products} onAddToCart ={handleAddToCart} /> */}
+                <Cart cart={cart}/>
+            </div>
+        </Router>
+       
+    );
 }
 
 export default App
